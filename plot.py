9f1267ce651c62399405f8234020df667757d623
e215760 <silentwiz@DESKTOP-MM0IPM0.localdomain>
2025-06-12 01:23:17 +0900

    wp

--- plot.py
+++ plot.py
@@ -5,15 +5,38 @@ import matplotlib.pyplot as plt
 ks = [100, 300, 500, 700, 1000, 1500, 2000]
 distance_errors = [0.19, 0.22, 0.16, 0.17, 0.15, 0.16, 0.16]
 execution_times = [0.3288, 0.4581, 0.3497, 0.3393, 0.3382, 0.3600, 0.4419]
+#0.173 m
+#0.374 sec
 '''
+
 '''
+# univ bf 5000-15000
+#==== image[IMG_2977.jpeg] ====
+ks = [ 5000, 7000, 9000, 11000, 13000, 15000]
+distance_errors = [ 0.43, 0.41, 0.40, 0.41, 0.41, 0.42]
+execution_times = [ 0.1916, 0.2691, 0.1813, 0.2390, 0.2485, 0.2024]
+# 0.413 m
+# 0.222 sec
+'''
+
 # univ bovw
 # ====== [IMG_3342.JPG] ========
 ks = [100, 300, 500, 700, 1000, 1500, 2000]
 distance_errors = [0.216, 0.1233, 0.12,  0.140,0,0,0.1333 ]
 execution_times = [0.487, 0.5029, 0.466, 0.497,0,0,0.5423 ]
+#0.147 m
+#0.499 sec
 
 '''
+# univ bovw 5000-15000
+#==== image[IMG_2977.jpeg] ====
+ks = [ 5000, 7000, 9000, 11000, 13000, 15000]
+distance_errors = [ 0.43, 0.41, 0.40, 0.41, 0.41, 0.42]
+execution_times = [ 0.1916, 0.2691, 0.1813, 0.2390, 0.2485, 0.2024]
+# 0.413 m
+# 0.222 sec
+'''
+
 
 '''
 # park bf
@@ -21,7 +44,10 @@ execution_times = [0.487, 0.5029, 0.466, 0.497,0,0,0.5423 ]
 ks = [100, 300, 500, 700, 1000, 1500, 2000]
 distance_errors = [0.41, 0.41, 0.44, 0.41, 0.40, 0.42, 0.40]
 execution_times = [0.1926, 0.1860, 0.2017, 0.2180, 0.2136, 0.2996, 0.2428]
+#0.413 m
+#0.222 sec
 '''
+
 
 '''
 # park bf 5000-15000
@@ -29,6 +55,8 @@ execution_times = [0.1926, 0.1860, 0.2017, 0.2180, 0.2136, 0.2996, 0.2428]
 ks = [ 5000, 7000, 9000, 11000, 13000, 15000]
 distance_errors = [ 0.43, 0.41, 0.40, 0.41, 0.41, 0.42]
 execution_times = [ 0.1916, 0.2691, 0.1813, 0.2390, 0.2485, 0.2024]
+# 0.413 m
+# 0.222 sec
 '''
 
 '''
@@ -37,20 +65,52 @@ execution_times = [ 0.1916, 0.2691, 0.1813, 0.2390, 0.2485, 0.2024]
 ks = [100, 300, 500, 700, 1000, 1500, 2000]
 distance_errors = [ 0, 0, 0.423, 0.420, 0.413, 0, 0]
 execution_times = [ 0, 0, 0.205, 0.172, 0.167, 0, 0]
+# 0.4187 m
+# 0.1813 sec
 '''
+
+
 '''
 # park bovw - 5000-15000
 #==== image[IMG_2977.jpeg] ====
 ks = [ 5000, 7000, 9000, 11000, 13000, 15000]
 distance_errors = [0.4125, 0.4217, 0.4150, 0.4117, 0.4100, 0.4183]
 execution_times = [0.2652, 0.2917, 0.3362, 0.3784, 0.4114, 0.4540]
+#0.415 m
+#0.356 sec
 '''
 
+'''
 plt.figure()
 plt.plot(ks, distance_errors, marker='o', color='red', label='Distance error (m)')
 plt.plot(ks, execution_times, marker='x', color='blue', label='Execution time (sec)')
 plt.xlabel('clustering number')
 plt.ylabel('Value')
+plt.xticks(ks)
+plt.legend()
+plt.grid(True)
+plt.savefig('./plot.png', dpi=300, bbox_inches='tight')
+#plt.show()
+'''
+
+
+
+plt.figure()
+plt.plot(ks, distance_errors, marker='o', color='red', label='Distance error (m)')
+plt.plot(ks, execution_times, marker='x', color='blue', label='Execution time (sec)')
+plt.xlabel('clustering number')
+plt.ylabel('Distance error (m)')
+plt.xticks(ks)
+plt.legend()
+plt.grid(True)
+plt.savefig('./plot.png', dpi=300, bbox_inches='tight')
+#plt.show()
+
+plt.figure()
+plt.plot(ks, distance_errors, marker='o', color='red', label='Distance error (m)')
+plt.plot(ks, execution_times, marker='x', color='blue', label='Execution time (sec)')
+plt.xlabel('clustering number')
+plt.ylabel('Execution time (sec)')
 plt.xticks(ks)
 plt.legend()
 plt.grid(True)
